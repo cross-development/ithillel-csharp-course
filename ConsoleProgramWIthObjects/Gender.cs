b29cedd63eb049314abc9862ab9659ff1adc6788
Vitaliy Derda <derdavitaliy@gmail.com>
2024-05-02 20:01:34 +0300

    feat: added console app with objects

---
+++ ConsoleProgramWIthObjects/Gender.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleProgramWIthObjects;
+
+/// <summary>
+/// User gender.
+/// </summary>
+public enum Gender
+{
+    /// <summary>
+    /// The gender of the user is male.
+    /// </summary>
+    Male = 1,
+
+    /// <summary>
+    /// The gender of the user is female.
+    /// </summary>
+    Female = 2,
+
+    /// <summary>
+    /// The gender of the user is unknown.
+    /// </summary>
+    Unknown = 3,
+}
